847980915afdf4c8e87e705a17ad9baeb9dcffb5
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-08-10 21:32:10 +0200

    feat: add event click to button

--- src/component/Expense.svelte
+++ src/component/Expense.svelte
@@ -2,16 +2,20 @@
     export let name = '';
     export let amount = 0;
     export let id;
+    let displayAmount = false;
+    function toggleAmount() { displayAmount = !displayAmount; }
 </script>
 
 <article class="single-expense">
     <div class="expense-info">
         <h2>{name}
-        <button class="amount-btn">
+        <button class="amount-btn" on:click={toggleAmount}>
             <i class="fas fa-caret-down"/>
         </button>
     </h2>
-    <h4>amount: ${amount}</h4>
+    {#if displayAmount}
+        <h4>amount: ${amount}</h4>
+    {/if}
     </div>
     <div class="expense-buttons">
         <button class="expense-btn edit-btn">
