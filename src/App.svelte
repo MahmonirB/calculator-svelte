d173d78949c44ce641799a091b7cb134cf71dbc6
M Bkh <37932243+MahmonirB@users.noreply.github.com>
2023-08-13 13:34:08 +0200

    feat: clear expenses list (#3)

--- src/App.svelte
+++ src/App.svelte
@@ -7,18 +7,33 @@
   function removeExpense(id) {
     expenses = expenses.filter(item => item.id !== id);
   }
+
   function edit(event) {
     const { id } = event.detail;
     // edit logic
   }
+
+  function clearExpenses() {
+    expenses = [];
+  }
+
   const contextObj = {
     remove: removeExpense,
     // other context props
   }
+
   setContext('context', contextObj);
 </script>
 
 <Navbar />
 <main class="main" >
   <ExpenseList {expenses} on:edit={edit} />
+  <button
+    type="button"
+    class={`btn btn-primary btn-block ${expenses.length ? '' : 'disabled'}`}
+    disabled={!expenses.length}
+    on:click={clearExpenses}
+  >
+    Clear All Expenses
+  </button>
 </main>
